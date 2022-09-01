18225a3f885ede8196e72e28a39a0ec97fe1c253
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-01 17:22:31 +0430

    useGetAllTasks custom hook was added and its props were passed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Tasks } from "./components/Tasks";
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useGetAllTasks from "./hooks/useGetAllTasks";
 
 const App = () => {
     const [showAddPage, setShowAddPage] = useState(false);
+    const tasks = useGetAllTasks();
+
     return (
         <>
             <Header showAddPage={showAddPage} setShowAddPage={setShowAddPage} />
             <Routes>
                 <Route path="/" element={<Navigate to={"/tasks"} />} />
-                <Route path="/tasks" element={<Tasks />} />
+                <Route path="/tasks" element={<Tasks tasks={tasks} />} />
                 <Route path="/tasks/add" element={<AddTask />} />
             </Routes>
         </>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,17 +1,17 @@
 import { CompletedTasks } from "./CompletedTasks";
 import { Task } from "./Task";
 
-export const Tasks = () => {
+export const Tasks = ({ tasks }) => {
     return (
         <main>
             <div
                 className={`grid my-5 p-5 items-center gap-y-6 transition-all duration-1000 linear animation animation-appear`}
             >
-                <Task />
-                <Task />
-                <Task />
+                {tasks.map((task) => (
+                    <Task key={task.id} title={task.title} />
+                ))}
+                <CompletedTasks />
             </div>
-            <CompletedTasks />
         </main>
     );
 };
