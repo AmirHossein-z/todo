2bb0b4825b77d292985a3102a63061263be69277
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-01 16:41:33 +0430

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Tasks } from "./components/Tasks";
+import { Header } from "./components/Header";
+import { AddTask } from "./components/AddTask";
+import { useEffect, useState } from "react";
+
 const App = () => {
+    const [showAddPage, setShowAddPage] = useState(false);
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <Header showAddPage={showAddPage} setShowAddPage={setShowAddPage} />
+            <Routes>
+                <Route path="/" element={<Navigate to={"/tasks"} />} />
+                <Route path="/tasks" element={<Tasks />} />
+                <Route path="/tasks/add" element={<AddTask />} />
+            </Routes>
+        </>
     );
 };
 
