ac431d5c6bd440bbdcc0429b4cb28cd8e2efb109
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-05 11:53:34 +0430

    a function for getting one task from API and a function for updating task info were created and createTask function was edited to add status to every task

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -7,7 +7,18 @@ export const getTasks = () => {
     return axios.get(url);
 };
 
+export const getTask = (taskId) => {
+    const url = `${URL}/tasks/${taskId}`;
+    return axios.get(url);
+};
+
 export const createTask = (task) => {
     const url = `${URL}/tasks`;
+    task = { ...task, status: false };
     return axios.post(url, task);
+};
+
+export const updateTask = (task, taskId) => {
+    const url = `${URL}/tasks/${taskId}`;
+    return axios.put(url, task);
 };
