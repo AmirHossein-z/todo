e7f8bbfc4ca69026c34accff663ba4834fe59735
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-05 11:56:20 +0430

    a new animation was added to Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 import loadingSVG from "../assets/loading.svg";
 export const Loading = () => {
     return (
-        <main className="flex justify-center mt-10 jump">
+        <main className="flex justify-center mt-10 animate-bounce">
             <img src={loadingSVG} alt="loading" className="w-44" />
         </main>
     );
