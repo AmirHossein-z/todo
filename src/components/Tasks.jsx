34679549d8ccde71d501c186f5ee1dd1b96a13a2
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-05 12:24:31 +0430

    tasks.jsx was edited for showing completed tasks and uncompleted tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,21 +2,33 @@ import { CompletedTasks } from "./CompletedTasks";
 import { Loading } from "./Loading";
 import { Task } from "./Task";
 
-export const Tasks = ({ tasks, loading }) => {
+export const Tasks = ({ tasks, setTasks, loading }) => {
     return (
         <>
             {loading ? (
                 <Loading />
             ) : (
                 <main>
-                    <div
-                        className={`grid my-5 p-5 items-center gap-y-6 transition-all duration-1000 linear animation animation-appear`}
-                    >
-                        {tasks.map((task) => (
-                            <Task key={task.id} title={task.title} />
-                        ))}
-                        <CompletedTasks />
+                    <div className="grid my-5 p-5 items-center gap-y-6 transition-all duration-1000 linear">
+                        {tasks.length > 0 ? (
+                            tasks.map((task) => {
+                                return !task.status ? (
+                                    <Task
+                                        key={task.id}
+                                        task={task}
+                                        status={task.status}
+                                        tasks={tasks}
+                                        setTasks={setTasks}
+                                    />
+                                ) : null;
+                            })
+                        ) : (
+                            <h3 className="text-xl text-customText animate-pulse justify-self-center">
+                                You still don't have any notes
+                            </h3>
+                        )}
                     </div>
+                    <CompletedTasks tasks={tasks} setTasks={setTasks} />
                 </main>
             )}
         </>
