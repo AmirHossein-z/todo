4daa32ff7e5f517f1b3c9435d4ff7ef1d860bf63
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-05 12:12:46 +0430

    CompletedTasks component renders Task component that has status completed

--- src/components/CompletedTasks.jsx
+++ src/components/CompletedTasks.jsx
@@ -1,8 +1,22 @@
-export const CompletedTasks = () => {
+import { Task } from "./Task";
+
+export const CompletedTasks = ({ tasks, setTasks }) => {
     return (
-        <div className="grid my-5 p-5 items-center gap-y-6 text-customText">
-            <h2 className="text-lg tracking-wider">Completed</h2>
-            {/* new completed Task should be created here! */}
+        <div className="grid my-5 p-5 items-center gap-y-6 transition-all duration-1000 linear">
+            <h2 className="text-lg text-customText tracking-wider font-bold">
+                Completed
+            </h2>
+            {tasks.map((task) => {
+                return task.status ? (
+                    <Task
+                        key={task.id}
+                        task={task}
+                        status={task.status}
+                        tasks={tasks}
+                        setTasks={setTasks}
+                    />
+                ) : null;
+            })}
         </div>
     );
 };
