f76f9080d628b5ee2ed2dc260d2996164f09ddf1
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-05 12:27:14 +0430

    a route for ViewTask component was added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Tasks } from "./components/Tasks";
-import { Header } from "./components/Header";
+import Header from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import { useState } from "react";
 import useGetAllTasks from "./hooks/useGetAllTasks";
+import { ViewTask } from "./components/ViewTask";
 
 const App = () => {
     const [loading, setLoading, tasks, setTasks] = useGetAllTasks();
-    const [task, setTask] = useState({});
+
     return (
         <>
             <Header />
@@ -15,19 +16,29 @@ const App = () => {
                 <Route path="/" element={<Navigate to={"/tasks"} />} />
                 <Route
                     path="/tasks"
-                    element={<Tasks tasks={tasks} loading={loading} />}
+                    element={
+                        <Tasks
+                            tasks={tasks}
+                            setTasks={setTasks}
+                            loading={loading}
+                        />
+                    }
                 />
                 <Route
                     path="/tasks/add"
                     element={
                         <AddTask
-                            task={task}
-                            setTask={setTask}
                             loading={loading}
                             setLoading={setLoading}
                             tasks={tasks}
                             setTasks={setTasks}
                         />
+                    }
+                />
+                <Route
+                    path="/tasks/:taskId"
+                    element={
+                        <ViewTask setLoading={setLoading} loading={loading} />
                     }
                 />
             </Routes>
