712129bc6e7e323e93d66ea173a1312da70e20f5
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-27 18:31:03 +0330

    error boundary component was added for showing errors when app is crashed

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import { RiErrorWarningFill } from "react-icons/ri";
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {}
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div className="bg-customdark text-customText grid gap-y-3 justify-center mt-56">
+                    <RiErrorWarningFill className="text-5xl sm:text-6xl md:text-7xl justify-self-center" />
+                    <p className="text-base sm:text-lg md:text-xl xl:text-2xl">
+                        Something went wrong!
+                    </p>
+                    <p className="justify-self-center font-bold md:text-lg">
+                        <i> try it later</i>
+                    </p>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "react-confirm-alert/src/react-confirm-alert.css";
 import "react-toastify/dist/ReactToastify.css";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
         <React.StrictMode>
-            <App />
+            <ErrorBoundary>
+                <App />
+            </ErrorBoundary>
         </React.StrictMode>
     </BrowserRouter>
 );
