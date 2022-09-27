de77d953be33f5375cf67e86f6e8f09f88c69b9e
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-09-27 18:31:33 +0330

    an icon & some style was added to not found component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import { TbError404 } from "react-icons/tb";
 
 const NotFound = () => {
     return (
-        <div className="bg-customdark grid justify-center gap-y-5 mt-24">
-            <p className="text-customText text-base sm:text-lg md:text-xl xl:text-2xl animate-bounce">
+        <div className="bg-customdark text-customText grid justify-center gap-y-3 mt-24">
+            <TbError404 className="text-5xl sm:text-6xl md:text-7xl justify-self-center" />
+            <p className="text-base sm:text-lg md:text-xl xl:text-2xl">
                 The page you wanted was not found
             </p>
             <Link
                 to="/tasks"
-                className="justify-self-center font-bold text-customText md:text-lg"
+                className="justify-self-center font-bold md:text-lg"
             >
                 <u>Go to main page</u>
             </Link>
